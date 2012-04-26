b5d71f3188c5b095787953d596b49972f4071519
Alexander Chernyakhovsky <achernya@mit.edu>
2012-04-26 19:21:50 -0400

    Add rudimentary test framework for AESCTR

---
+++ src/test/TestAESCTR.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('..')
+
+import unittest
+
+from crypto import *
+
+class TestAESCTR(unittest.TestCase):
+
+    def setUp(self):
+        (key, nonce) = BlockCipher.generate_ivs()
+        self.enc = AESCTR(key, nonce)
+        self.dec = AESCTR(key, nonce)
+
+    def test_canDecryptOwnCrypto(self):
+        plaintext = "Does PyCrypto actually work in AES-CTR mode?"
+        ciphertext = self.enc.encrypt(plaintext)
+        decrypttext = self.dec.decrypt(ciphertext)
+        self.assertTrue(plaintext == decrypttext)
+
+if __name__ == '__main__':
+    unittest.main()
